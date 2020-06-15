90a2d3a20dd2e83f139211322c65a5d411436bf9
panzehong <2917179196@qq.com>
2020-06-15 17:57:39 +0800

    1

--- UI循环列表/LoopList/Assets/Case1/Scripts/ItemMessages.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ItemMessages
-{
-    public List<Content> Contents { get; set; }
-}
---
+++ UI循环列表/LoopList/Assets/ScrollFlow/Case1/Scripts/Content.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Content
+{
+    public string PicUrl { get; set; }
+    public string Date { get; set; }
+    public string Message { get; set; }
+}
